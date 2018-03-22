6d327deedb884223fbd35374abe772e9adec4f71
markrosslonergan <markrosslonergan@gmail.com>
2018-03-22 19:19:01 -0400

    Tidying up /inc a bit

--- inc/SBNmultisim.h
+++ inc/SBNmultisim.h
@@ -40,78 +40,70 @@ class SBNmultisim : public SBNconfig{
 	
 
 	public:
-	double tolerence_positivesemi;
-	int universes_used;
-	int Nfiles;
-	double abnormally_large_weight;
-
-	std::vector<std::string> variations;
-	
-
+		
 	SBNspec spec_CV;	
 	SBNspec spec_sig;
 
-	//these are vectors so 1 for each file
 	std::vector<TH2D> h_spec2d_CV;
 	std::vector<TH2D> h_spec2d_sig;
 	
-
-	SBNspec template_spec;	
-	SBNspec spec_CV2;	
+	
 	SBNmultisim(std::string xmlname);
 
-	// a vector of num_multisim vectors, with a vector of subchannel*bin histograms in each	
-	std::vector<SBNspec> multi_sbnspec;
-	std::vector<std::vector<double>> multi_vecspec;
-	std::vector<std::vector<double>> multi_vecspec2D;
-	std::vector<double> vecspec2DCV;
 	int formCovarianceMatrix(std::string fileout);
 	int writeOut();
+	int printMatricies(std::string fileout);
+	virtual bool eventSelection(int file);
+	virtual int fillHistograms(int file, int uni, double wei);
+
+	std::vector<SBNspec> multi_sbnspec;
+	std::vector<std::vector<double>> multi_vecspec;
 
 	TMatrixD full_covariance;
 	TMatrixD frac_covariance;
 	TMatrixD full_correlation;
 
-	TMatrixD full_covariance2D;
-	TMatrixD frac_covariance2D;
-	TMatrixD full_correlation2D;
-
-
-
+	//for plotting, hsa been superseeded by printMatricies a bit
 	TH2D * hist_frac_cov;
-	TH2D * hist_full_cor;// = new TH2D("Corr","",num_bins_total,1,num_bins_total, num_bins_total,1,num_bins_total);
-	TH2D * hist_full_cov;// = new TH2D("Full Cov","",num_bins_total,1,num_bins_total, num_bins_total,1,num_bins_total);
+	TH2D * hist_full_cor;
+	TH2D * hist_full_cov;
+
+	//Some checks on multisims
+	double tolerence_positivesemi;
+	int universes_used;
+	double abnormally_large_weight;
 
 
 
+	//Multisim input variables
+	std::vector<std::string> variations;
 	std::vector<std::vector<double> > vars;
+
+	int Nfiles;
+	std::vector<TFile *> files;
+	std::vector<TTree *> trees;
+
+	std::vector<int> nentries;
+	std::vector< TBranch *> * bWeight;
+	std::vector< std::map<std::string, std::vector<double> > * > * fWeights;
 
 	std::vector<std::vector<int> > vars_i;
 	std::vector<std::vector<double> > vars_d;
 
-	std::vector<TFile *> files;
-	std::vector<TTree *> trees;
-
-
-	std::vector<int> nentries;
-
-	std::vector< TBranch *> * bWeight;
-	std::vector< TBranch *> * bLepMom;
-
-	std::vector< std::map<std::string, std::vector<double> > * > * fWeights;
-	std::vector<TLorentzVector * > *fLepMom;
 
 
 
-	virtual bool eventSelection(int file);
-	virtual int fillHistograms(int file, int uni, double wei);
-
-	//one for each file
-
-
-	int printMatricies(std::string fileout);
-
+	// In testing 2D fits with 4D covariance..
+	SBNspec template_spec;	
+	SBNspec spec_CV2;	
 	THnSparseD *frac_covariance_4d;
+	TMatrixD full_covariance2D;
+	TMatrixD frac_covariance2D;
+	TMatrixD full_correlation2D;
+	std::vector<std::vector<double>> multi_vecspec2D;
+	std::vector<double> vecspec2DCV;
+
+
 
 };
 
