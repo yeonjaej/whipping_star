9244f660926586d29468e6b590b20cbaf087a235
Mark Ross-Lonergan <markrosslonergan@gmail.com>
2018-09-06 13:55:53 -0500

    setup.sh made for gpvm compliation

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+source /grid/fermiapp/products/uboone/setup_uboone.sh; 
+setup git v2_15_1; 
+setup gitflow v1_11_0; 
+setup mrb v1_16_02;
+setup root v6_08_06g -q e14:nu:prof; 
+setup cmake v2_8_8;
+
+
 me="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $me
 
@@ -5,3 +13,7 @@ export SBNFITDIR=$me
 export SBNFIT_LIBDIR=$me/build
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SBNFIT_LIBDIR
+
+
+
+
