d11750ae71bbafae49936df3827d7e048f8cc0db
Victor Genty <vgenty@icsubmit01.sdcc.bnl.gov>
2018-09-17 13:18:18 -0400

    save

--- examples/example_3.cxx
+++ examples/example_3.cxx
@@ -42,65 +42,65 @@ using namespace sbn;
 
 int main(int argc, char* argv[])
 {
-	std::string xml = "example.xml";
-	int iarg = 0;
-	opterr=1;
-	int index;
-	bool sample_from_covariance = false;
-	int num_MC_events = 40000;
+  std::string xml = "example.xml";
+  int iarg = 0;
+  opterr=1;
+  int index;
+  bool sample_from_covariance = false;
+  int num_MC_events = 40000;
 
-	const struct option longopts[] =
+  const struct option longopts[] =
+    {
+      {"xml", 		required_argument, 	0, 'x'},
+      {"covariance", 		no_argument,0,'c'},
+      {"number", 		required_argument,	0,'n'},
+      {0,			no_argument, 		0,  0},
+    };
+
+  while(iarg != -1)
+    {
+      iarg = getopt_long(argc,argv, "x:n:c", longopts, &index);
+
+      switch(iarg)
 	{
-		{"xml", 		required_argument, 	0, 'x'},
-		{"covariance", 		no_argument,0,'c'},
-		{"number", 		required_argument,	0,'n'},
-		{0,			no_argument, 		0,  0},
-	};
-
-	while(iarg != -1)
-	{
-		iarg = getopt_long(argc,argv, "x:n:c", longopts, &index);
-
-		switch(iarg)
-		{
-			case 'x':
-				xml = optarg;
-				break;
-			case 'c':
-				sample_from_covariance = true;
-				break;
-			case 'n':
-				num_MC_events = (int)strtod(optarg,NULL);
-				break;
-			case '?':
-			case 'h':
-				std::cout<<"Allowed arguments:"<<std::endl;
-				std::cout<<"\t-x\t--xml\t\tInput .xml file for SBNconfig"<<std::endl;
-				std::cout<<"\t-c\t--covariance\t\tSample from covariance matrix instead of Poisson"<<std::endl;
-				std::cout<<"\t-n\t--number\t\tNumber of MC events for frequentist studies (default 40k)"<<std::endl;
-				return 0;
-		}
+	case 'x':
+	  xml = optarg;
+	  break;
+	case 'c':
+	  sample_from_covariance = true;
+	  break;
+	case 'n':
+	  num_MC_events = (int)strtod(optarg,NULL);
+	  break;
+	case '?':
+	case 'h':
+	  std::cout<<"Allowed arguments:"<<std::endl;
+	  std::cout<<"\t-x\t--xml\t\tInput .xml file for SBNconfig"<<std::endl;
+	  std::cout<<"\t-c\t--covariance\t\tSample from covariance matrix instead of Poisson"<<std::endl;
+	  std::cout<<"\t-n\t--number\t\tNumber of MC events for frequentist studies (default 40k)"<<std::endl;
+	  return 0;
 	}
+    }
 
-	std::string tag = "EXAMPLE3";
+  std::string tag = "EXAMPLE3";
 
-    	SBNspec sig("EXAMPLE1.SBNspec.root",xml);
-	sig.Scale("leesignal",2.5);
+  SBNspec sig("EXAMPLE1.SBNspec.root",xml);
+  sig.Scale("leesignal",2.5);
 	
-	SBNspec bkg("EXAMPLE1.SBNspec.root",xml);
-	bkg.Scale("leesignal",0.0);
+  SBNspec bkg("EXAMPLE1.SBNspec.root",xml);
+  bkg.Scale("leesignal",0.0);
 
-	// Stats + sys
-	TFile * fsys = new TFile("EXAMPLE1.SBNcovar.root","read");
-	TMatrixD * cov = (TMatrixD*)fsys->Get("frac_covariance_EXAMPLE1");
+  // Stats + sys
+  TFile * fsys = new TFile("EXAMPLE1.SBNcovar.root","read");
+  TMatrixD * cov = (TMatrixD*)fsys->Get("frac_covariance_EXAMPLE1");
 
-	SBNchi *chi = new SBNchi(bkg,*cov);
-	SBNchi *chi_statonly = new SBNchi(bkg);
+  SBNchi *chi = new SBNchi(bkg,*cov);
+  SBNchi *chi_statonly = new SBNchi(bkg);
 
-	SBNcls cls_factory(&sig, &bkg,*cov);
-	if(sample_from_covariance) cls_factory.SetSampleCovariance();
+  SBNcls cls_factory(&sig, &bkg,*cov);
+  if(sample_from_covariance) cls_factory.SetSampleCovariance();
 
-	cls_factory.CalcCLS(num_MC_events, tag);
+  cls_factory.CalcCLS(num_MC_events, tag);
 
-	return 0;
+  return 0;
 }
